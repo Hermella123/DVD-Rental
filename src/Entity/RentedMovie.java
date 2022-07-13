17ecfab4a9dc20a4143a770710841704d0d0a5fd
Hermella Kebede <hermellatadele5@gmail.com>
2022-07-13 14:12:35 +0200

    Constructing Rented-movie class

--- src/Entity/RentedMovie.java
+++ src/Entity/RentedMovie.java
@@ -1,4 +1,39 @@
 package Entity;
 
+import java.util.Date;
+
 public class RentedMovie {
+    private Date rentalDate;
+    private Date returnDate;
+    private Movie_copie rentedMovie;
+
+    public RentedMovie(Date rentalDate, Date returnDate, Movie_copie rentedMovie) {
+        this.rentalDate = rentalDate;
+        this.returnDate = returnDate;
+        this.rentedMovie = rentedMovie;
+    }
+
+    public Date getRentalDate() {
+        return rentalDate;
+    }
+
+    public void setRentalDate(Date rentalDate) {
+        this.rentalDate = rentalDate;
+    }
+
+    public Date getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(Date returnDate) {
+        this.returnDate = returnDate;
+    }
+
+    public Movie_copie getRentedMovie() {
+        return rentedMovie;
+    }
+
+    public void setRentedMovie(Movie_copie rentedMovie) {
+        this.rentedMovie = rentedMovie;
+    }
 }
