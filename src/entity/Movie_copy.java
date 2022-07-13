1db9c11d794bbbc8b4f82fe4fa891a50a40e3d81
Hermella Kebede <hermellatadele5@gmail.com>
2022-07-13 16:17:49 +0200

    creating user manager service

--- src/entity/Movie_copy.java
+++ src/entity/Movie_copy.java
@@ -1,4 +1,4 @@
-package Entity;
+package entity;
 
 public class Movie_copy {
     private long copiedMovieId;
---
+++ src/services/UserManager.java
@@ -0,0 +1,30 @@
+package services;
+
+import entity.Movie_copy;
+import entity.User;
+
+import java.util.List;
+
+public class UserManager {
+    public void addUser(User user){
+
+    }
+    public void removeUser(User user){
+
+    }
+    public User getUser( String firstName){
+       return null;
+    }
+    public List<User> getUserList(){
+        return null;
+    }
+    public void addRentedMovie(User user, Movie_copy movieCopy){
+        //return null;
+    }
+    public void removeRentedMovie(User user, long movieId){
+        //return null;
+    }
+    public List<Movie_copy> getRentedMovie(User user){
+        return null;
+    }
+}
