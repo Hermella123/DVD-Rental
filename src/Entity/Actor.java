a4f3d97546d532f8fb361bd729682e593d4f0edf
Hermella Kebede <hermellatadele5@gmail.com>
2022-07-13 14:28:16 +0200

    Constructing the Actors class

--- src/Entity/Actor.java
+++ src/Entity/Actor.java
@@ -1,4 +1,49 @@
 package Entity;
 
+import java.util.Date;
+
 public class Actor {
+    private long actorId;
+    private String firstName;
+    private String lastName;
+    private Date birthDate;
+
+    public Actor(long actorId, String firstName, String lastName, Date birthDate) {
+        this.actorId = actorId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDate = birthDate;
+    }
+
+    public long getActorId() {
+        return actorId;
+    }
+
+    public void setActorId(long actorId) {
+        this.actorId = actorId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
 }
