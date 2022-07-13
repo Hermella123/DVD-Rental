60d18d516abff8ac9c940de193d64f8d8d4a0342
Hermella Kebede <hermellatadele5@gmail.com>
2022-07-13 14:19:14 +0200

    Constructing copy of the movie class

---
+++ src/Entity/Movie_copy.java
@@ -0,0 +1,37 @@
+package Entity;
+
+public class Movie_copy {
+    private long copiedMovieId;
+    private boolean available;
+    private Movie movie;
+
+    public Movie_copy(long copiedMovieId, boolean available, Movie movie) {
+        this.copiedMovieId = copiedMovieId;
+        this.available = available;
+        this.movie = movie;
+    }
+
+    public long getCopiedMovieId() {
+        return copiedMovieId;
+    }
+
+    public void setCopiedMovieId(long copiedMovieId) {
+        this.copiedMovieId = copiedMovieId;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    public Movie getMovie() {
+        return movie;
+    }
+
+    public void setMovie(Movie movie) {
+        this.movie = movie;
+    }
+}
