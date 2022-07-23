e4b59b9a5c389202c6045b0b672b61374fd84d56
Hermella Kebede <hermellatadele5@gmail.com>
2022-07-23 17:30:35 +0200

    Cleaning th e program start page code

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -1,5 +1,6 @@
 import controller.MovieController;
 import controller.UserController;
+import services.DatabaseConnectionManager;
 
 import java.util.Scanner;
 
@@ -8,16 +9,15 @@ public class Program {
     public static UserController userController;
     public static void main(String []args){
         Program program= new Program();
+        DatabaseConnectionManager.connect();
         program.menu();
     }
     public void menu(){
         int input;
         Scanner scan= new Scanner(System.in);
         System.out.println("\t\tMovie Management \n\t\t please input number only press" +
-                "\t\t1. To movies Menu" +
-                "\t\t2. To User Menu" +
-                "\t\t6. To rent movie" +
-                "\t\t3. To view movie detail" );
+                "\t\t1. Movies Menu" +
+                "\t\t2. User Menu"  );
         input = scan.nextInt();
         switch (input){
             case 1:
@@ -32,5 +32,6 @@ public class Program {
                 System.out.println("wrong entry please insert only number between 1 upto 7");
                 menu();
         }
+        menu();
     }
 }
